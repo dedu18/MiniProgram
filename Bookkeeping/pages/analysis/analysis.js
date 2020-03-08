a0781418c2c9dcc10cc97ce555ac5dae3b5fed17
Android <swpuxzh@163.com>
2020-03-08 21:10:42 +0800

    分析页

--- Bookkeeping/pages/analysis/analysis.js
+++ Bookkeeping/pages/analysis/analysis.js
@@ -1,66 +1,140 @@
-// pages/analysis/analysis.js
-Page({
+var wxCharts = require('../../component/wx-echars/wxcharts.js'); //引入wxChart文件
+var app = getApp();
+var lineChart = null;
 
+Page({
   /**
    * 页面的初始数据
    */
-  data: {
+  data: {},
+  pieShow: function(data) {
+    new wxCharts({
+      animation: true, //是否有动画
+      canvasId: 'pieCanvas',
+      type: 'pie',
+      series: [{
+        name: '成交量1',
+        data: 15,
+      }, {
+        name: '成交量2',
+        data: 35,
+      }, {
+        name: '成交量3',
+        data: 78,
+      }],
+      width: 300,
+      height: 200,
+      dataLabel: true,
+    });
 
+    new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: ['2012', '2013', '2014', '2015', '2016', '2017'],
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: '成交量1',
+        data: [0.15, null, 0.45, 0.37, 0.4, 0.8],
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }, {
+        name: '成交量2',
+        data: [2, 0, 0, 3, null, 4, 0, 0, 2, 0],
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: '成交金额 (万元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      width: 300,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    new wxCharts({
+      animation: true, //是否有动画
+      canvasId: 'pieCanvas',
+      type: 'pie',
+      series: [{
+        name: '成交量1',
+        data: 15,
+      }, {
+        name: '成交量2',
+        data: 35,
+      }, {
+        name: '成交量3',
+        data: 78,
+      }],
+      width: 300,
+      height: 200,
+      dataLabel: true,
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
