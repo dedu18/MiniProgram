64f094c3f4ae0a42ac1443072054a80422f5c99c
Android <swpuxzh@163.com>
2020-03-09 12:24:25 +0800

    分析页面

--- Bookkeeping/pages/analysis/analysis.js
+++ Bookkeeping/pages/analysis/analysis.js
@@ -1,14 +1,16 @@
 var wxCharts = require('../../component/wx-echars/wxcharts.js'); //引入wxChart文件
-var app = getApp();
-var lineChart = null;
-
+var windowW = 0;
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    date: "2020-02",
+    income: 10000.0.toFixed(2),
+    expense: 2000.0.toFixed(2)
+  },
   pieShow: function(data) {
-    new wxCharts({
+    var option = {
       animation: true, //是否有动画
       canvasId: 'pieCanvas',
       type: 'pie',
@@ -23,70 +25,23 @@ Page({
         data: 78,
       }],
       width: 300,
-      height: 200,
+      height: 300,
       dataLabel: true,
-    });
-
-    new wxCharts({
-      canvasId: 'lineCanvas',
-      type: 'line',
-      categories: ['2012', '2013', '2014', '2015', '2016', '2017'],
-      animation: true,
-      background: '#f5f5f5',
-      series: [{
-        name: '成交量1',
-        data: [0.15, null, 0.45, 0.37, 0.4, 0.8],
-        format: function (val, name) {
-          return val.toFixed(2) + '万';
-        }
-      }, {
-        name: '成交量2',
-        data: [2, 0, 0, 3, null, 4, 0, 0, 2, 0],
-        format: function (val, name) {
-          return val.toFixed(2) + '万';
-        }
-      }],
-      xAxis: {
-        disableGrid: true
-      },
-      yAxis: {
-        title: '成交金额 (万元)',
-        format: function (val) {
-          return val.toFixed(2);
-        },
-        min: 0
-      },
-      width: 300,
-      height: 200,
-      dataLabel: false,
-      dataPointShape: true,
-      extra: {
-        lineStyle: 'curve'
-      }
-    });
+    }
+    new wxCharts(option)
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    new wxCharts({
-      animation: true, //是否有动画
-      canvasId: 'pieCanvas',
-      type: 'pie',
-      series: [{
-        name: '成交量1',
-        data: 15,
-      }, {
-        name: '成交量2',
-        data: 35,
-      }, {
-        name: '成交量3',
-        data: 78,
-      }],
-      width: 300,
-      height: 200,
-      dataLabel: true,
+    // 屏幕宽度
+    this.setData({
+      imageWidth: wx.getSystemInfoSync().windowWidth
     });
+    console.log(this.data.imageWidth);
+
+    //计算屏幕宽度比列
+    windowW = this.data.imageWidth / 375;
   },
 
   /**
@@ -100,7 +55,74 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    /* 收入饼状图*/
+    new wxCharts({
+      animation: true, //是否有动画
+      canvasId: 'pieCanvas',
+      type: 'pie',
+      series: [{
+        name: '工资',
+        data: 5000,
+      }, {
+        name: '红包',
+        data: 35,
+      }],
+      width: (375 * windowW),
+      height: (250 * windowW),
+      dataLabel: true,
+    })
+    /* 支出饼状图*/
+    var option = {
+      animation: true, //是否有动画
+      canvasId: 'pieCanvas1',
+      type: 'pie',
+      series: [{
+        name: '京东',
+        data: 15,
+      }, {
+        name: '超市',
+        data: 35,
+      }, {
+        name: '淘宝',
+        data: 78,
+      }],
+      width: (375 * windowW),
+      height: (250 * windowW),
+      dataLabel: true,
+    }
+    new wxCharts(option)
+    /**支出折线图 */
+    new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: ['03-01', '03-02', '03-03', '03-04', '03-05', '03-06'],
+      animation: true,
+      background: '#f5f5f5',
+      series: [{
+        name: '支出金额',
+        data: [100, 200, 125, 2, 15, 22],
+        format: function (val, name) {
+          return val.toFixed(2);
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: '支出金额 (元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      width: (375 * windowW),
+      height: (250 * windowW),
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
   },
 
   /**
