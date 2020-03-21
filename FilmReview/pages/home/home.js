df1aaf992af89b221387d8b5ef2da89662dc9bc6
Android <swpuxzh@163.com>
2020-03-21 12:09:48 +0800

    个人页面

--- FilmReview/pages/home/home.js
+++ FilmReview/pages/home/home.js
@@ -5,62 +5,82 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {
+      nickName: '未登陆',
+      avatarUrl: '../../images/head.jpg'
+    },
+    login: false,
+    mode: ['我的收藏', '我的历史', '联系客服', '关于我们']
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let that = this
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res)
+              that.setData({
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- FilmReview/pages/search/search.js
+++ FilmReview/pages/search/search.js
@@ -5,62 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    searchload: false,
+    searchpage: true,
+    hotKeyword: ['我仍然相信', '喋血战士', '隐形人', '狩猎', '刺猬索尼克', '回归之路', '野性的呼唤', '爱玛'],
+    hotTag: ['豆瓣高分', '冷门佳片', '华语', '欧美', '韩国', '日本', '动作'],
+    searchresult:[]
   },
-
+  search: function(e) {
+    this.setData({
+      searchload: true
+    })
+    this.setData({
+      searchpage: false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
